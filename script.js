06790d4746f048ac8367d33e1e4fe86d4d8516fc
Mavi-star12 <moreira.luz.maria@escola.pr.gov.br>
2026-06-12 19:07:36 -0300

    script.js

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-window.onload = function() {
-    const botao = document.getElementById("btnVoltarTopo");
-    
-    if (botao) {
-        botao.onclick = function() {
-            window.scrollTo({
-                top: 0,
-                behavior: 'smooth'
-            });
+<button id="btnVoltarTopo" class="btn-sim" style="position: fixed; bottom: 20px; right: 20px; z-index: 9999;">
+        ↑ Voltar ao Início
+    </button>
+
+    <script>
+        window.onload = function() {
+            const botao = document.getElementById("btnVoltarTopo");
+            
+            if (botao) {
+                botao.onclick = function() {
+                    window.scrollTo({
+                        top: 0,
+                        behavior: 'smooth'
+                    });
+                };
+            } else {
+                console.error("Erro: O botão com o ID 'btnVoltarTopo' não foi encontrado no HTML.");
+            }
         };
-    } else {
-        console.error("Erro: O botão com o ID 'btnVoltarTopo' não foi encontrado no HTML.");
-    }
-};
+    </script>
+</body>
